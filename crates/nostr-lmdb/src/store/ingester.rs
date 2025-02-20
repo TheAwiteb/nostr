146852ddd0b259b44b386d650a8bdee2f3158ef9
Awiteb <a@4rs.nl>
2025-02-20 12:20:24 +0000

    chore: Remove DBs changes
    Signed-off-by: Awiteb <a@4rs.nl>

--- crates/nostr-lmdb/src/store/ingester.rs
+++ crates/nostr-lmdb/src/store/ingester.rs
@@ -6,7 +6,6 @@ use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
 
 use heed::RwTxn;
-use nostr::filter::{Alphabet, Filter, SingleLetterTag};
 use nostr::nips::nip01::Coordinate;
 use nostr::{Event, Kind, Timestamp};
 use nostr_database::{FlatBufferBuilder, RejectedReason, SaveEventStatus};
@@ -193,8 +192,6 @@ impl Ingester {
                 txn.abort();
                 return Ok(SaveEventStatus::Rejected(RejectedReason::InvalidDelete));
             }
-        } else if event.kind == Kind::RequestToVanish {
-            self.handle_request_to_vanish(&mut txn, &event)?;
         }
 
         // Store and index the event
@@ -246,26 +243,5 @@ impl Ingester {
         read_txn.commit()?;
 
         Ok(false)
-    }
-
-    fn handle_request_to_vanish(&self, txn: &mut RwTxn, event: &Event) -> nostr::Result<(), Error> {
-        // Acquire read txn
-        let read_txn = self.db.read_txn()?;
-
-        // Delete author events
-        self.db
-            .delete(&read_txn, txn, Filter::new().author(event.pubkey))?;
-        // Delete Gift Wraps that p-tagged the author
-        self.db.delete(
-            &read_txn,
-            txn,
-            Filter::new().kind(Kind::GiftWrap).custom_tag(
-                SingleLetterTag::lowercase(Alphabet::P),
-                event.pubkey.to_hex(),
-            ),
-        )?;
-
-        read_txn.commit()?;
-        Ok(())
     }
 }
