317654efef608a57983fdab1218d0dce814d81ec
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-06-17 09:53:22 +0200

    relay-pool-sansio: init
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

---
+++ crates/nostr-relay-pool-sansio/src/prelude.rs
@@ -0,0 +1,11 @@
+// Copyright (c) 2022-2023 Yuki Kishimoto
+// Copyright (c) 2023-2025 Rust Nostr Developers
+// Distributed under the MIT software license
+
+//! Prelude
+
+#![allow(unknown_lints)]
+#![allow(ambiguous_glob_reexports)]
+#![doc(hidden)]
+
+pub use crate::relay::{self, *};
